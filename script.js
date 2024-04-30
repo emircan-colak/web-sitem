280fbe02e43f53bdecd5d9d2be6ee769ba84f787
Emircan Colak <emircancolak508@gmail.com>
2024-04-30 10:40:51 +0300

    site değişti

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function openRightbar() {
+    document.querySelector(".rightbar").style.width = "450px";
+    document.querySelector(".content2").style.marginRight = "250px";
+}
+
+function closeRightbar() {
+    document.querySelector(".rightbar").style.width = "0";
+    document.querySelector(".content2").style.marginRight= "0";
+}
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('sidebar-open');
+});
+
+
+
+
